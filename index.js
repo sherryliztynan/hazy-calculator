3a106aaa658701ceab92e8a5ee894275696b725c
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-07-25 17:53:00 -0400

    for loop and first test conidition for null

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function calculateArray(array) {
   let nums = []
   let operand = []
 }
+ // if string "null" push 0 to nums array//
+ for (var i = 0; i < array.length; i++) {
+    if (array[i] === null) {
+      nums.push(0)
+    }
