fcd54987b534173e13d7efbada49e1eaa80ae52c
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-07-25 17:54:24 -0400

    second condition for test for stringified numbers

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function calculateArray(array) {
   let nums = []
   let operand = []
-}
- // if string "null" push 0 to nums array//
- for (var i = 0; i < array.length; i++) {
+
+  // if string "null" push 0 to nums array//
+  for (var i = 0; i < array.length; i++) {
     if (array[i] === null) {
       nums.push(0)
     }
+    // if string "12" or 12 push to nums array//    
+    else if (!isNaN(array[i]) && (array[i]) !== '') {
+      nums.push(parseInt(array[i]))
+    }
+  }
+}
